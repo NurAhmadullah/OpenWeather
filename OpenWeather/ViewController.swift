f23d9e12152f41f14eef94d5530ece6a7f6ee1c8
nur.ahmadullah <nur.ahmadullah@bjitgroup.com>
2023-10-16 11:38:16 +0600

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by BJIT on 16/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
